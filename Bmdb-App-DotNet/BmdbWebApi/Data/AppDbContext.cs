adac43c39f4e3938e4762e261a4c627e5469e6d0
mchilders14 <childersmatt4@gmail.com>
2021-07-15 00:10:09 -0400

    init commit

---
+++ Bmdb-App-DotNet/BmdbWebApi/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BmdbWebApi.Models;
+
+namespace BmDbWebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BmdbWebApi.Models.Actor> Actors { get; set; }
+        public DbSet<BmdbWebApi.Models.Movie> Movies { get; set; }
+        public DbSet<BmdbWebApi.Models.Credit> Credits { get; set; }
+        public DbSet<BmdbWebApi.Models.User> Users { get; set; }
+        public DbSet<BmdbWebApi.Models.MovieCollection> MovieCollections { get; set; }
+    }
+}
---
+++ Bmdb-App-DotNet/BmdbWebApi/Models/MovieCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BmdbWebApi.Models
+{
+    public class MovieCollection
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal PurchasePrice { get; set; }
+
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}
